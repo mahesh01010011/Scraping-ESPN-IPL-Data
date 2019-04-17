ed5a2a08a367ed0c570c8dbd9dd8732a830fc1c3
mahesh01010011 <8641836+mahesh01010011@users.noreply.github.com>
2019-04-17 07:24:35 +0530

    Match summary from downloaded HTML files
    Added code to extract the match summary (total, wickets, extras) from the downloaded HTML files. Also, instead of creating a DataFrame object in the beginning and then subsequently appending more DataFrame objects to it, I've created a list to contain the DataFrame objects. These are then concatenated (pd.concat) at the end.

--- extract_data.py
+++ extract_data.py
@@ -6,50 +6,88 @@ from bs4 import BeautifulSoup
 from tqdm import tqdm
 
 
-SUBFOLDER_HTML = 'Downloads'
-SUBFOLDER_SUMMARY = 'Match Results'
-
-innings_id = ('gp-inning-00', 'gp-inning-01')
-
-df_batting = pd.DataFrame()
-df_bowling = pd.DataFrame()
-
-for season in tqdm(os.listdir(f'{SUBFOLDER_SUMMARY}')[::-1]):
-    with open(f'{SUBFOLDER_SUMMARY}/{season}', 'r') as fr:
-        matches = list(csv.reader(fr))[1:][::-1]
+def parse_summary(inn_summary, extras, match_id, innings):
+    tmp = [i.split() for i in extras.upper()[:-1].split('(')[-1].split(',')]
+    if extras == '0':
+        extras = dict()
+    else:
+        extras = {k: int(v) for k, v in tmp}
     
-    for match in tqdm(matches):
-        file_name = match[-1].split('/')[-1]
-        if match[3] in ('abandoned', 'no result', '-'): # '-' is for live match
-            continue
+    tmp = inn_summary.split()
+    rr = float(tmp[-1][:-1])
         
-        with open(f'{SUBFOLDER_HTML}/{file_name}', 'rb') as fr:
-            soup = BeautifulSoup(fr.read(), 'lxml')
-        match_id = file_name.replace('.html', '')
+    overs = float(tmp[-4][1:])
+    score = tmp[0].split('/')
+    wickets = int(score[1]) if len(score) > 1 else 10
+    
+    return pd.Series({
+        'Match ID': match_id,
+        'Innings': innings,
+        'Total': int(score[0]),
+        'Wickets': wickets,
+        'Overs': overs,
+        'RR': rr,
+        **extras,
+    })
+
+if __name__ == '__main__':
+    SUBFOLDER_HTML = 'Downloads'
+    SUBFOLDER_SUMMARY = 'Match Results'
+    
+    dfs_batting = []
+    dfs_bowling = []
+    summaries = []
+    
+    innings_id = ('gp-inning-00', 'gp-inning-01')
+    
+    for season in tqdm(os.listdir(f'{SUBFOLDER_SUMMARY}')[::-1]):
+        with open(f'{SUBFOLDER_SUMMARY}/{season}', 'r') as fr:
+            matches = list(csv.reader(fr))[1:][::-1]
         
-        for k, inning_id in enumerate(innings_id, start=1):
-            inning = soup.find('div', {'id': inning_id})
+        for match in tqdm(matches):
+            file_name = match[-1].split('/')[-1]
+            if match[3] in ('abandoned', 'no result', '-'): # '-' is for live match
+                continue
             
-            sec_batting = inning.find('div', {'class': 'scorecard-section batsmen'})
-            data_batting = [['Match ID', 'Innings', 'Player ID'] + \
-                [div.text for div in sec_batting.find('div', {'class': 'wrap header'}).find_all('div')[:-1]]] # last div is empty
-            for i in sec_batting.find_all('div', {'class': 'wrap batsmen'}):
-                tmp = i.find_all('div')
-                data_batting.append([match_id, k, tmp[0].a['href'].split('/')[-1][:-5]] + \
-                    [div.text for div in tmp[:-1]]) # last div is empty
+            with open(f'{SUBFOLDER_HTML}/{file_name}', 'rb') as fr:
+                soup = BeautifulSoup(fr.read(), 'lxml')
+            match_id = file_name.replace('.html', '')
             
-            sec_bowling = inning.find('div', {'class': 'scorecard-section bowling'}).table
-            data_bowling = [['Match ID', 'Innings', 'Player ID'] + \
-                [th.text for th in sec_bowling.thead.tr.find_all('th') if len(th.text) > 0]]
-            for tr in sec_bowling.tbody.find_all('tr'):
-                tmp = tr.find_all('td')
-                data_bowling.append([match_id, k, tmp[0].a['href'].split('/')[-1][:-5]] + \
-                    [td.text for td in tmp if len(td.text) > 0])
-            
-            df_batting = df_batting.append(pd.DataFrame(data_batting[1:], columns=data_batting[0]), ignore_index=True, sort=False)
-            df_bowling = df_bowling.append(pd.DataFrame(data_bowling[1:], columns=data_bowling[0]), ignore_index=True, sort=False)
-
-df_batting.rename(columns={'': 'Commentary', 'BATSMEN': 'Batsman'}, inplace=True)
-
-df_batting.to_csv('Batting.csv', index=False)
-df_bowling.to_csv('Bowling.csv', index=False)
+            for inning, inning_id in enumerate(innings_id, start=1):
+                div_inning = soup.find('div', {'id': inning_id})
+                
+                # Batting
+                div_batting = div_inning.find('div', {'class': 'scorecard-section batsmen'})
+                data_batting = [['Match ID', 'Innings', 'Player ID'] + \
+                    [div.text for div in div_batting.find('div', {'class': 'wrap header'}).find_all('div')[:-1]]] # last div is empty
+                for i in div_batting.find_all('div', {'class': 'wrap batsmen'}):
+                    tmp = i.find_all('div')
+                    data_batting.append([match_id, inning, tmp[0].a['href'].split('/')[-1][:-5]] + \
+                        [div.text for div in tmp[:-1]]) # last div is empty
+                
+                # Bowling
+                div_bowling = div_inning.find('div', {'class': 'scorecard-section bowling'}).table
+                data_bowling = [['Match ID', 'Innings', 'Player ID'] + \
+                    [th.text for th in div_bowling.thead.tr.find_all('th') if len(th.text) > 0]]
+                for tr in div_bowling.tbody.find_all('tr'):
+                    tmp = tr.find_all('td')
+                    data_bowling.append([match_id, inning, tmp[0].a['href'].split('/')[-1][:-5]] + \
+                        [td.text for td in tmp if len(td.text) > 0])
+                
+                dfs_batting.append(pd.DataFrame(data_batting[1:], columns=data_batting[0]))
+                dfs_bowling.append(pd.DataFrame(data_bowling[1:], columns=data_bowling[0]))
+                
+                # Summary
+                extras = div_inning.find('div', {'class': 'wrap extras'}).find_all('div')[-1].text
+                inn_summary = div_inning.find('div', {'class': 'wrap total'}).find_all('div')[-1].text
+                summaries.append(parse_summary(inn_summary, extras, match_id, inning))
+    
+    df_batting = pd.concat(dfs_batting, sort=False)
+    df_bowling = pd.concat(dfs_bowling, sort=False)
+    df_misc = pd.DataFrame(summaries)
+    
+    df_batting.rename(columns={'': 'Commentary', 'BATSMEN': 'Batsman'}, inplace=True)
+    
+    df_batting.to_csv('Batting.csv', index=False)
+    df_bowling.to_csv('Bowling.csv', index=False)
+    df_misc.to_csv('Summary.csv', index=False)
